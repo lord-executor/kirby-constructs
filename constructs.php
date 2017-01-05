c6af29d70ea9fb9be2c21a5d3c00ab731c8c9125
Lukas Angerer <lukas.angerer@gmail.com>
2017-01-05 12:22:13 +0100

    Removed prefix from construct namespace mapping

--- constructs.php
+++ constructs.php
@@ -6,7 +6,7 @@ $loader->addPsr4('Constructs\\', __DIR__ . DS . 'src');
 foreach (c::get('constructs.dirs', ['Constructs' => 'site/constructs']) as $name => $constructDir) {
 	$dir = $kirby->roots()->index . DS . $constructDir;
 	if (is_dir($dir . DS . 'src')) {
-		$loader->addPsr4('Constructs\\' . $name . '\\', $dir . DS . 'src');
+		$loader->addPsr4($name . '\\', $dir . DS . 'src');
 	}
 }
 
