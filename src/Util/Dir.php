1c724bdd6abbebf22384a3053c845284770b7a34
Lukas Angerer <lukas.angerer@gmail.com>
2017-01-06 18:24:32 +0100

    Code commehts for utility classes

--- src/Util/Dir.php
+++ src/Util/Dir.php
@@ -5,11 +5,20 @@ namespace Constructs\Util;
 use Obj;
 
 
+/**
+ * Helper class that facilitates working with and going through directory structures, finding specific files, etc.
+ */
 class Dir
 {
 	protected $path;
 	protected $entries;
 
+	/**
+	 * Creates a new Dir instance with the given directory path.
+	 *
+	 * @param string $path
+	 *   Path to a file system directory.
+	 */
 	public function __construct($path)
 	{
 		$this->path = $path;
@@ -23,11 +32,21 @@ class Dir
 		}
 	}
 
+	/**
+	 * Gets an Enumerable collection of all the files (no sub-directories) within this directory.
+	 *
+	 * @return Enumerable<Obj>
+	 */
 	public function files()
 	{
 		return $this->entries->filter([static::class, 'filterFiles']);
 	}
 
+	/**
+	 * Gets an Enumerable collection of all the sub-directories (no files) within this directory.
+	 *
+	 * @return Enumerable<Obj>
+	 */
 	public function dirs()
 	{
 		return $this->entries
@@ -35,11 +54,25 @@ class Dir
 			->map([static::class, 'extendDir']);
 	}
 
+	/**
+	 * Gets an Enumerable of all files and sub-directories within this directory.
+	 *
+	 * @return Enumerable<Obj>
+	 */
 	public function all()
 	{
 		return $this->entries;
 	}
 
+	/**
+	 * Recursivel finds all files within the current directory and it's descendants that match the given predicate.
+	 *
+	 * @param callable $predicate
+	 *   A file matching predicate with the signature 'function (Obj) { return bool; }' that takes a directory entry
+	 *   and returns true/false depending on whether the file should match or not.
+	 *
+	 * @return Enumerable
+	 */
 	public function find($predicate)
 	{
 		return Enumerable::fromArray($this->findInternal($predicate, $this->path));
--- src/Util/Enumerable.php
+++ src/Util/Enumerable.php
@@ -2,26 +2,51 @@
 
 namespace Constructs\Util;
 
-
 use ArrayObject;
 use IteratorAggregate;
 use Traversable;
 
 
+/**
+ * Enumerable helper class to deal with PHP's complete lack of reasonable map & filter APIs. - to be clear, PHP
+ * does have map and filter APIs, they are just not at all _reasonable_.
+ */
 class Enumerable implements IteratorAggregate
 {
-	public static function fromArray($arr)
+	/**
+	 * Creates an Enumerable from an array.
+	 *
+	 * @param array $arr
+	 *   The array.
+	 *
+	 * @return Enumerable
+	 */
+	public static function fromArray(array $arr)
 	{
 		return new Enumerable(new ArrayObject($arr));
 	}
 
 	protected $inner;
 
+	/**
+	 * Creates an Enumerable from any IteratorAggregate implementation (anything with a getIterator function).
+	 *
+	 * @param IteratorAggregate $inner
+	 *   The underlying iterable object.
+	 */
 	public function __construct(IteratorAggregate $inner)
 	{
 		$this->inner = $inner;
 	}
 
+	/**
+	 * Creates a new Enumerable by applying the mapping function $func to each element of the current Enumerable.
+	 *
+	 * @param callable $func
+	 *   Mapping function with the signature 'function (mixed) { return mixed; }'
+	 *
+	 * @return Enumerable
+	 */
 	public function map($func)
 	{
 		$result = [];
@@ -33,6 +58,15 @@ class Enumerable implements IteratorAggregate
 		return new Enumerable(new ArrayObject($result));
 	}
 
+	/**
+	 * Creates a new Enumerable that only contains the elements from the current Enumerable that match the given
+	 * $predicate function.
+	 *
+	 * @param callable $predicate
+	 *   Predicate function with the signature 'function (mixed) { return bool; }'
+	 *
+	 * @return Enumerable
+	 */
 	public function filter($predicate)
 	{
 		$result = [];
